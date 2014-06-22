a48fb5531d2a3b5ce748baf190fbf433f360692c
Chris Hyndman <chrishyndman@outlook.com>
2014-06-22 12:35:13 -0700

    Accepts path parameter, instead of extracting to working directory

--- scripts/extract-zip.ps1
+++ scripts/extract-zip.ps1
@@ -1,9 +1,10 @@
 $cd = (split-path $MyInvocation.MyCommand.Path -parent)
 $zipPath = $args[0]
-Write-Host " * Extracting files into $cd..." -foregroundcolor "green"
+$zipDest = $args[1]
+Write-Host " * Extracting files into $zipDest..." -foregroundcolor "green"
 $shell = New-Object -com Shell.Application
 $zipFile = $shell.NameSpace($zipPath)
 foreach($item in $zipFile.items())
 {
-	$shell.Namespace($cd).copyhere($item)
+	$shell.Namespace($zipDest).copyhere($item)
 }
