15568700e90770c555d5f4422b3c7dbe724f2c6b
Chris Hyndman <chrishyndman@outlook.com>
2014-06-25 15:10:23 -0700

    Need to create destination directory in order to extract

--- scripts/extract-zip.ps1
+++ scripts/extract-zip.ps1
@@ -1,10 +1,17 @@
 $cd = (split-path $MyInvocation.MyCommand.Path -parent)
 $zipPath = $args[0]
 $zipDest = $args[1]
+
+if (!(Test-Path $zipDest))
+{
+	Write-Host " * Creating $zipDest..." -foregroundcolor "green"
+	New-Item $zipDest -type directory -force
+}
+
 Write-Host " * Extracting files into $zipDest..." -foregroundcolor "green"
 $shell = New-Object -com Shell.Application
 $zipFile = $shell.NameSpace($zipPath)
-foreach($item in $zipFile.items())
+foreach ($item in $zipFile.items())
 {
 	$shell.Namespace($zipDest).copyhere($item)
 }
