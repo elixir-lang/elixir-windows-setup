339b09267358ae8dda469a871acd0c57c604d8ba
Chris Hyndman <chrishyndman@outlook.com>
2014-07-02 15:19:43 -0700

    Remove info messages from extract-zip.ps1

--- scripts/extract-zip.ps1
+++ scripts/extract-zip.ps1
@@ -4,11 +4,9 @@ $zipDest = $args[1]
 
 if (!(Test-Path $zipDest))
 {
-	Write-Host " * Creating $zipDest..." -foregroundcolor "green"
 	New-Item $zipDest -type directory -force
 }
 
-Write-Host " * Extracting files into $zipDest..." -foregroundcolor "green"
 $shell = New-Object -com Shell.Application
 $zipFile = $shell.NameSpace($zipPath)
 foreach ($item in $zipFile.items())
