1349c09d1f51c84d4a8ec38197bf81d6c59ddad3
Chris Hyndman <chrishyndman@outlook.com>
2014-07-07 12:51:36 -0700

    Remove scripts except for make-installer.ps1 in new location

--- scripts/set-env.ps1
+++
@@ -1,8 +0,0 @@
-$elixirRoot = $args[0]
-$elixirBin = ($elixirRoot + "\bin").Replace("\\", "\")
-
-Write-Host " * Appending $elixirBin to Path variable..." -foregroundcolor "green"
-if (!($env:Path.Contains($elixirBin)))
-{
-	[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $elixirBin, "Machine")
-}
